222908981387009fe16e24fafe61f54757a5297a
Noel <jnoelbentinganan@gmail.com>
2025-05-23 00:00:26 +1000

    Initial commit 5-23-25

---
+++ app.py
@@ -0,0 +1,13 @@
+print("#######" * 5)
+print("## Welcome to CatNameGenerator! ###")
+print("#######" * 5)
+print("-------" * 5)
+
+color = input("What is your favorite color?\n")
+food = input("What is your favorite food?\n")
+
+print("Your cat name is: " + str(color) + " " + str(food) +"!")
+
+
+# Day One:
+# Learned about print, input functions, use variables, str() method, string concatenation
